d02302279f393d177ccff2f9fda6922c4a5eb4a3
Mario Rodriguez <marioa.rodriguez@msn.com>
2020-10-06 01:01:40 -0600

    added background color changer

--- force-app/main/default/aura/ChangeColorPage/ChangeColorPageController.js
+++ force-app/main/default/aura/ChangeColorPage/ChangeColorPageController.js
@@ -1,7 +1,16 @@
 ({
-    handleChange : function(component, event) {
-        var selectedOptionValue = event.getParam("value");
-        //alert("Option selected with value: '" + selectedOptionValue + "'");
-        console.log(selectedOptionValue);
+    handleChange : function(component, event,helper) {
+        var selectedOptionValue = event.getParam("value");                
+        if(selectedOptionValue == "rojo"){                    
+            helper.addstyleRed(component, event);           
+        } else if(selectedOptionValue == "amarillo"){            
+            helper.addstyleYellow(component, event);           
+        } else if(selectedOptionValue == "azul"){            
+            helper.addstyleBlue(component, event);                      
+        }
+    },
+    colorChanger : function(component,event,helper){
+        var pickedColor = component.find("colorPicker").get("v.value");
+        console.log(pickedColor);
     }
 })
--- force-app/main/default/aura/ChangeColorPage/ChangeColorPageHelper.js
+++ force-app/main/default/aura/ChangeColorPage/ChangeColorPageHelper.js
@@ -1,5 +1,20 @@
 ({
-    helperMethod : function() {
-
-    }
+    addstyleRed : function(component, event, helper) {
+        var targetComponent = component.find("theContainer");
+        $A.util.removeClass(targetComponent, 'externalYellow');
+        $A.util.removeClass(targetComponent, 'externalBlue');
+        $A.util.addClass(targetComponent,'externalRed');
+    },
+    addstyleYellow : function(component, event, helper) {
+        var targetComponent = component.find("theContainer");
+        $A.util.removeClass(targetComponent, 'externalBlue');
+        $A.util.removeClass(targetComponent, 'externalRed');
+        $A.util.addClass(targetComponent,'externalYellow');
+    },
+    addstyleBlue : function(component, event, helper) {
+        var targetComponent = component.find("theContainer");
+        $A.util.removeClass(targetComponent, 'externalRed');
+        $A.util.removeClass(targetComponent, 'externalYellow');
+        $A.util.addClass(targetComponent,'externalBlue');
+    }    
 })
