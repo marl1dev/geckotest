51ccb6e5708ff01c510f26502fae19209dd59005
Mario Rodriguez <marioa.rodriguez@msn.com>
2020-10-06 22:33:31 -0600

    added form to create records

--- force-app/main/default/aura/CaseFormPage/CaseFormPageController.js
+++ force-app/main/default/aura/CaseFormPage/CaseFormPageController.js
@@ -1,5 +1,32 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doSave : function(component, event, helper) {
+        var nombre = component.get("v.nombre");
+        var apellido = component.get("v.apellido");
+        var nombreempresa = component.get("v.nombrempresa");        
+        var correoElectronico = component.get("v.correoelectronico");
+        var descripcion = component.get("v.descripcioncaso");
+        console.log(nombre + ' ' + apellido);
+        var action = component.get("c.createAccounts");
+        action.setParams({
+            "nombre" : nombre,
+            "apellido" : apellido, 
+            "nombreempresa" : nombreempresa,
+            "correo" : correoElectronico,
+            "descripcion" : descripcion
+        }); 
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                helper.showToast('Exito!', 'Registros creados satifactoriamente', 'success');
+                component.set("v.nombre",null);
+                component.set("v.apellido",null);
+                component.set("v.nombrempresa",null);
+                component.set("v.correoelectronico",null);
+                component.set("v.descripcioncaso",null);
+            }else{
+                console.log(state);
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
--- force-app/main/default/aura/CaseFormPage/CaseFormPageHelper.js
+++ force-app/main/default/aura/CaseFormPage/CaseFormPageHelper.js
@@ -1,5 +1,12 @@
 ({
-    helperMethod : function() {
-
+    showToast : function(title, message, error) {
+        let toastParams = {
+            title: title,
+            message: message, // Error message
+            type: error
+        };
+        let toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams(toastParams);
+        toastEvent.fire();
     }
 })
